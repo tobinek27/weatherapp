12890bdac9754b865e9a14d8e7e9e2eb236eac38
tobinek27 <tobias.haltmar@gmail.com>
2025-01-07 17:15:09 +0100

    added weather fetching functionality

--- WeatherApp/DashboardForm.Designer.cs
+++ WeatherApp/DashboardForm.Designer.cs
@@ -6,8 +6,11 @@ namespace WeatherApp
     partial class DashboardForm
     {
         private IContainer components = null;
-        private Label lblWelcome; // welcome label
-        private Button btnLogout; // logout button
+        private Label lblWelcome; // Welcome label
+        private Button btnLogout; // Logout button
+        private ComboBox cmbCountry; // Dropdown for country selection
+        private Button btnFetchWeather; // Button to fetch weather
+        private TextBox txtWeatherInfo; // Textbox to display weather info
 
         protected override void Dispose(bool disposing)
         {
@@ -25,18 +28,21 @@ namespace WeatherApp
             this.components = new System.ComponentModel.Container();
             this.lblWelcome = new System.Windows.Forms.Label();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.btnFetchWeather = new System.Windows.Forms.Button();
+            this.txtWeatherInfo = new System.Windows.Forms.TextBox();
 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "DashboardForm";
 
             this.lblWelcome.AutoSize = true;
-            this.lblWelcome.Location = new System.Drawing.Point(300, 200);
+            this.lblWelcome.Location = new System.Drawing.Point(300, 50);
             this.lblWelcome.Name = "lblWelcome";
-            this.lblWelcome.Size = new System.Drawing.Size(0, 17);
+            this.lblWelcome.Size = new System.Drawing.Size(200, 20);
             this.lblWelcome.TabIndex = 0;
 
-            this.btnLogout.Location = new System.Drawing.Point(350, 250);
+            this.btnLogout.Location = new System.Drawing.Point(350, 400);
             this.btnLogout.Name = "btnLogout";
             this.btnLogout.Size = new System.Drawing.Size(100, 40);
             this.btnLogout.TabIndex = 1;
@@ -44,8 +50,33 @@ namespace WeatherApp
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.BtnLogout_Click);
 
+            this.cmbCountry.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.cmbCountry.Location = new System.Drawing.Point(300, 100);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(200, 28);
+            this.cmbCountry.TabIndex = 2;
+
+            this.btnFetchWeather.Location = new System.Drawing.Point(350, 150);
+            this.btnFetchWeather.Name = "btnFetchWeather";
+            this.btnFetchWeather.Size = new System.Drawing.Size(100, 30);
+            this.btnFetchWeather.TabIndex = 3;
+            this.btnFetchWeather.Text = "Get Weather";
+            this.btnFetchWeather.UseVisualStyleBackColor = true;
+            this.btnFetchWeather.Click += new System.EventHandler(this.BtnFetchWeather_Click);
+
+            this.txtWeatherInfo.Location = new System.Drawing.Point(150, 200);
+            this.txtWeatherInfo.Multiline = true;
+            this.txtWeatherInfo.Name = "txtWeatherInfo";
+            this.txtWeatherInfo.ReadOnly = true;
+            this.txtWeatherInfo.ScrollBars = ScrollBars.Vertical;
+            this.txtWeatherInfo.Size = new System.Drawing.Size(500, 150);
+            this.txtWeatherInfo.TabIndex = 4;
+
             this.Controls.Add(this.lblWelcome);
             this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.btnFetchWeather);
+            this.Controls.Add(this.txtWeatherInfo);
 
             this.ResumeLayout(false);
             this.PerformLayout();
--- WeatherApp/DashboardForm.cs
+++ WeatherApp/DashboardForm.cs
@@ -1,11 +1,72 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace WeatherApp
 {
     public partial class DashboardForm : Form
     {
+        private readonly User _user;
+
         public DashboardForm(User user)
         {
             InitializeComponent();
+            _user = user;
             lblWelcome.Text = $"Welcome to the dashboard, {user.Username}!";
+            PopulateCountries();
+        }
+
+        private void PopulateCountries()
+        {
+            var countries = new[] { "Czechia", "USA", "Canada", "Germany", "France", "Spain", "India", "Japan" };
+            cmbCountry.Items.AddRange(countries);
+            cmbCountry.SelectedIndex = 0;
+        }
+
+        private async void BtnFetchWeather_Click(object sender, EventArgs e)
+        {
+            string selectedCountry = cmbCountry.SelectedItem?.ToString();
+
+            if (string.IsNullOrWhiteSpace(selectedCountry))
+            {
+                MessageBox.Show("Please select a country first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            btnFetchWeather.Text = "Fetching...";
+            btnFetchWeather.Enabled = false;
+            txtWeatherInfo.Text = "Please wait, fetching weather...";
+
+            try
+            {
+                string weatherData = await FetchWeatherAsync(selectedCountry);
+                txtWeatherInfo.Text = weatherData;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while fetching weather: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                btnFetchWeather.Text = "Get Weather";
+                btnFetchWeather.Enabled = true;
+            }
+        }
+
+        private async Task<string> FetchWeatherAsync(string country)
+        {
+            string apiUrl = $"https://wttr.in/{Uri.EscapeDataString(country)}?format=4";
+            using HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed to fetch weather data. Please try again later.");
+            }
+
+            return await response.Content.ReadAsStringAsync();
         }
 
         private void BtnLogout_Click(object sender, EventArgs e)
